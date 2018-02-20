fbb3ec336e3dce45c5276e3303a709972bca02a6
Marcel <marcel.sko@gmx.com>
2018-02-21 00:51:49 +0800

    Added Riot Routes

---
+++ routes/editor.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/editor', function (req, res) {
+    res.render('editor', { title: 'Editor' });
+});
+
+module.exports = router;
---
+++ routes/prototype.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+router.get('/prototype', function (req, res) {
+    res.render('prototype/index', { title: 'Prototype' });
+});
+
+module.exports = router;
